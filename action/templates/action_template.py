aa64ffbd82591a00da021e94876b5343a7fe2a3e
the-py-guy <54561694+the-py-guy@users.noreply.github.com>
2019-08-27 01:26:06 -0400

    Add files via upload

---
+++ action/templates/action_template.py
@@ -0,0 +1,23 @@
+import argparse, base64, json, asyncio, websockets
+
+async def request(uri, data):
+	async with websockets.connect(uri) as websocket:
+		await websocket.send(data)
+		return dict(json.loads(await websocket.recv()))
+
+def ai_predict(data):
+	return asyncio.get_event_loop().run_until_complete(
+		request('ws://localhost:8765', data))
+
+
+def main(data):
+	print('looks like you havent programmed me to do this yet. heres some data: '+str(data))
+		
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', required=True)
+args = parser.parse_args()
+wrapped = args.data
+unwrapped = base64.b64decode(wrapped.encode('utf-8'))
+call_json = json.loads(unwrapped.decode('utf-8'))
+main(call_json)
