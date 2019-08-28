eb5c0ba8f04ad11a49e6f8002e6bebca3c97a791
the-py-guy <54561694+the-py-guy@users.noreply.github.com>
2019-08-28 17:28:52 -0400

    Add files via upload

--- action/add_new_action.py
+++ action/add_new_action.py
@@ -1,4 +1,21 @@
-import argparse, json, random, string, base64
+import argparse, json, random, string, base64, asyncio, websockets
+
+def confirm_deny_cancel(inp):
+	prediction = ai_predict(inp)['prediction_data']
+	if prediction['certainty'] > 0.7:
+		if prediction['tag'] == 'cancel':
+			exit()
+		else:
+			return prediction['tag']
+
+async def request(uri, data):
+	async with websockets.connect(uri) as websocket:
+		await websocket.send(data)
+		return dict(json.loads(await websocket.recv()))
+
+def ai_predict(data):
+	return asyncio.get_event_loop().run_until_complete(
+		request('ws://localhost:8765', data))
 
 def create_action(trigger):
 	f_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
@@ -16,8 +33,18 @@ def create_action(trigger):
 
 def main(data):
 	while True:
-		trigger = input('what action should i add?')
-		create_action(trigger)
+		print('what action should i create?')
+		action = input('you: ')
+		confirm_deny_cancel(action)
+		print('create action '+action+'?')
+		inp = input('you: ')
+		if confirm_deny_cancel(inp) == 'confirm':
+			create_action(action)
+			print('ok. i created a new action for you.')
+			exit()
+		elif confirm_deny_cancel(inp) == 'deny':
+			print('sorry. lets try that again.')
+		
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', required=True)
